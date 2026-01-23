9a1c19c5585c5a0003ff0073f2eb0d13fe7576c5
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-01-23 10:08:21 +0000

    one word per line

---
+++ misc/one_word_perline.c
@@ -0,0 +1,41 @@
+//To print one word per line
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define IN 1
+#define OUT 0
+#define MAX_LEN 1000
+
+int main(void){
+    char str[MAX_LEN];
+    bool state = OUT;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    int i = 0;
+    while (str[i] != '\0') {
+        if ((str[i] == ' ' || str[i] == '\t') && state == IN) {
+            state = OUT;
+            printf("\n");
+        } else if (!state && (str[i] != ' ' && str[i] != '\t')) {
+            state = IN;
+        }
+        if (state) {
+            printf("%c", str[i]);
+        }
+        i++;
+
+    }
+    printf("\n");
+
+    return 0;
+}
+            
+
+
+
+
